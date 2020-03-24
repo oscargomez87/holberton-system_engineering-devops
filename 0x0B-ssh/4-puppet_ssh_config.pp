f0f84021b859909b7b0090e83d391b437992e1e2
oscargomez87 <oscarstiven87@gmail.com>
2020-03-24 09:07:25 -0500

    Changed path for "turn off passw auth"

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -4,7 +4,7 @@ include stdlib
 
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => '/etc/ssh/ssh_config',
+  path   => '/etc/ssh/sshd_config',
   line   => 'PasswordAuthentication no',
 }
 
