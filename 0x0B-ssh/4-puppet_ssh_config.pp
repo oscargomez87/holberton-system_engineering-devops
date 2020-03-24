30e6133f0ff06e97ef56942a14e840b6b218d2cd
oscargomez87 <oscarstiven87@gmail.com>
2020-03-24 09:28:59 -0500

    Aded spaces to line

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -3,13 +3,13 @@
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  match  => '^PasswordAuthentication',
-  line   => 'PasswordAuthentication no',
+  match  => '^ PasswordAuthentication',
+  line   => ' PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => present,
-  match  => '^IdentityFile',
+  match  => '^ IdentityFile',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton',
+  line   => ' IdentityFile ~/.ssh/holberton',
 }
