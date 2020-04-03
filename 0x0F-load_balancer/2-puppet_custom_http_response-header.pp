ac8da9a9d98df243c1f7595816c6f727b9a1b3c9
oscargomez87 <oscarstiven87@gmail.com>
2020-04-03 16:11:18 -0500

    Added installation of package and service start

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,8 +1,26 @@
-# Configures ssh client to use specific pvkey and not use password
+# Configures clean ubuntu 16.04 with nginx
+
+exec { 'apt-get-update':
+  command     => '/usr/bin/apt-get update',
+}
+
+package { 'Install nginx':
+  name     => 'nginx',
+  provider => apt,
+  ensure   => installed,
+  require  => Exec['apt-get-update'],
+}
+
 
 file_line { 'Add header':
-  ensure => present,
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => '^\s*location \/ {',
-  line   => "\t\tadd_header X-Served-By \$hostname;",
+  ensure  => present,
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => '^\s*location \/ {',
+  line    => "\t\tadd_header X-Served-By \$hostname;",
+  require => Exec['Install nginx'],
+}
+
+service { 'nginx':
+  ensure   => true,
+  require  => File_line['Add header'],
 }
