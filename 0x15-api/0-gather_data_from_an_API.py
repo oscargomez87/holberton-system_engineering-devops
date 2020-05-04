4657c9b0e551b49a0d70ed9ecd8f88294f82fbdc
oscargomez87 <oscarstiven87@gmail.com>
2020-05-04 15:47:38 -0500

    Removed requirement of id to be an int

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    emp_id = int(argv[1])
+    emp_id = argv[1]
     url_todos = 'https://jsonplaceholder.typicode.com/todos'
     url_users = 'https://jsonplaceholder.typicode.com/users'
     todos_num_done = 0
