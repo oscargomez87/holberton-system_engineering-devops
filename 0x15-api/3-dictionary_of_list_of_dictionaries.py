906bdbae8f22b63240250908ce6e0c784ca15304
oscargomez87 <oscarstiven87@gmail.com>
2020-05-04 17:15:22 -0500

    Fixed list storing all tasks no matter the user

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,9 +9,9 @@ import requests
 if __name__ == '__main__':
     url_employees = 'https://jsonplaceholder.typicode.com/users/'
     res_employees = requests.get(url_employees)
-    todo_list = []
     todos_json = {}
     for employee in res_employees.json():
+        todo_list = []
         url_todos = url_employees + str(employee.get('id')) + '/todos/'
         res_todos = requests.get(url_todos)
         for todos in res_todos.json():
@@ -20,7 +20,7 @@ if __name__ == '__main__':
             todos.update({'task': todos.pop('title')})
             todos.update({'username': employee.get('username')})
             todo_list.append(todos)
-            todos_json.update({'{}'.format(employee.get('id')): todo_list})
+        todos_json.update({'{}'.format(employee.get('id')): todo_list})
     json_file = 'todo_all_employees.json'
     with open(json_file, 'w') as f:
         json.dump(todos_json, f)
