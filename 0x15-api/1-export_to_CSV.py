5fb49a64cd608de5f53530f58f90732421a921fd
oscargomez87 <oscarstiven87@gmail.com>
2020-05-04 14:37:36 -0500

    Extends script 0-gather_data_from_an_API and exports info to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Extends script 0-gather_data_from_an_API and exports info to csv
+
+    Parameters:
+        id = id of employee to find todos
+"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    emp_id = int(argv[1])
+    url_todos = 'https://jsonplaceholder.typicode.com/todos'
+    url_users = 'https://jsonplaceholder.typicode.com/users'
+    todos_num_done = 0
+    todos_task_done = []
+    res_user = requests.get(url_users, params={'id': emp_id})
+    res_todos = requests.get(url_todos, params={'userId': emp_id})
+    emp_uname = res_user.json()[0]['username']
+    csv_file = '{}.csv'.format(emp_id)
+    with open(csv_file, 'w') as f:
+        for todos in res_todos.json():
+            f.write('"{}","{}","{}","{}"\n'
+                    .format(todos['userId'], emp_uname, todos['completed'], todos['title']))
