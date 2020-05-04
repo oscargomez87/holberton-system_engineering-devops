62bcd253d8abe743a87b59c7e764ca21df925213
oscargomez87 <oscarstiven87@gmail.com>
2020-05-04 16:21:09 -0500

    Changed API usage, removed use of params

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,17 +11,17 @@ from sys import argv
 
 if __name__ == '__main__':
     emp_id = int(argv[1])
-    url_todos = 'https://jsonplaceholder.typicode.com/todos'
-    url_users = 'https://jsonplaceholder.typicode.com/users'
+    url_users = 'https://jsonplaceholder.typicode.com/users/{}/'.format(emp_id)
+    url_todos = url_users + 'todos/'
     todos_num_done = 0
     todos_task_done = []
-    res_user = requests.get(url_users, params={'id': emp_id})
-    res_todos = requests.get(url_todos, params={'userId': emp_id})
+    res_user = requests.get(url_users)
+    res_todos = requests.get(url_todos)
     for todos in res_todos.json():
         if todos['completed']:
             todos_num_done = todos_num_done + 1
             todos_task_done.append(todos['title'])
-    employee_name = res_user.json()[0]['name']
+    employee_name = res_user.json().get('name')
     print('Employee {} is done with tasks({}/{}):'
           .format(employee_name, todos_num_done, len(res_todos.json())))
     for task in todos_task_done:
