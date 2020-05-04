686edb86e92f21d92cc014bdee275b633616990d
oscargomez87 <oscarstiven87@gmail.com>
2020-05-04 15:49:54 -0500

    Fixed mismatch in string output Added strict int check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    emp_id = argv[1]
+    emp_id = int(argv[1])
     url_todos = 'https://jsonplaceholder.typicode.com/todos'
     url_users = 'https://jsonplaceholder.typicode.com/users'
     todos_num_done = 0
@@ -22,7 +22,7 @@ if __name__ == '__main__':
             todos_num_done = todos_num_done + 1
             todos_task_done.append(todos['title'])
     employee_name = res_user.json()[0]['name']
-    print('Employee {} is done with tasks {}/{}'
+    print('Employee {} is done with tasks({}/{}):'
           .format(employee_name, todos_num_done, len(res_todos.json())))
     for task in todos_task_done:
         print('\t{}'.format(task))
